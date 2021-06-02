6f697eac98ab55eb672339f8e2fd30b2f948aa2a
Jens Weise <jens.weise@ovgu.de>
2021-06-02 16:22:32 +0200

    Renamed Helper, added Logging Framework, refactored pom.xml

--- src/main/java/anno/Main.java
+++ src/main/java/anno/Main.java
@@ -1,29 +1,23 @@
 package anno;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-import java.sql.Types;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.eclipse.rdf4j.model.IRI;
-import org.eclipse.rdf4j.model.Literal;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.rdf4j.model.Model;
-import org.eclipse.rdf4j.model.Statement;
-import org.eclipse.rdf4j.model.Value;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.rio.RDFFormat;
 import org.eclipse.rdf4j.rio.RDFParseException;
 import org.eclipse.rdf4j.rio.Rio;
 import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+	private static final Logger LOG = LogManager.getLogger(Main.class);
 
 	
 	public static void main(String[] args) throws RDFParseException, UnsupportedRDFormatException, FileNotFoundException, IOException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException, NoSuchMethodException, SecurityException {
@@ -35,12 +29,17 @@ public class Main {
 		classes.add(Book.class);
 		classes.add(Offer.class);
 		
-		List<Object> objects = Helper.createInstances("book.ttl", classes);
-		
+		List<Object> objects = PojoRdfHelper.createInstances("book.ttl", classes);
+
+		LOG.debug("Test of conversion FROM RDF TO Java");
+
 		for(Object o: objects) {
-			System.out.println(o.toString());
+//			System.out.println(o.toString());
+			LOG.debug(o.toString());
 		}
 		
-		Helper.convertToRDF(objects);
+		String test = PojoRdfHelper.convertToRDF(objects,false);
+		LOG.debug("Test of conversion FROM JAVA TO RDF");
+		LOG.debug(test);
 	}
 }
---
+++ src/main/java/anno/PojoRdfHelper.java
@@ -0,0 +1,151 @@
+package anno;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
+
+public class PojoRdfHelper {
+
+    public static Object getDatatype(Type type, String cur) {
+        if (type.getTypeName() == "java.lang.Double") {
+            return Double.parseDouble(cur);
+        }
+        return cur;
+    }
+
+    public static String convertToRDF(List<Object> objects) throws IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, IOException, NoSuchMethodException, SecurityException {
+        return convertToRDF(objects, false);
+    }
+
+
+    public static String convertToRDF(List<Object> objects, boolean writeToFile) throws IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException, IOException, NoSuchMethodException,
+			SecurityException {
+        ModelBuilder mb = new ModelBuilder();
+        for (Object o : objects) {
+            Class c = o.getClass();
+            RDFObject objectAnnotation = (RDFObject) c.getAnnotation(RDFObject.class);
+            RDFSubject subjectAnnotation = (RDFSubject) c.getAnnotation(RDFSubject.class);
+            RDFNamespaces namespaces = (RDFNamespaces) c.getAnnotation(RDFNamespaces.class);
+            mb.setNamespace(namespaces.value()[0], namespaces.value()[1]);
+
+            Method methods[] = c.getDeclaredMethods();
+            Method getID = c.getDeclaredMethod("getId");
+            String subject = (String) getID.invoke(o);
+            for (Method m : methods) {
+                RDF methodAnnotation = m.getAnnotation(RDF.class);
+                if (methodAnnotation != null && m.getParameterCount() < 1) {
+                    mb.add(subject, methodAnnotation.value().toString(), m.invoke(o));
+                } else {
+                    continue;
+                }
+            }
+        }
+        Model model = mb.build();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Rio.write(model, byteArrayOutputStream, RDFFormat.TURTLE);
+        if (writeToFile) {
+			try (FileOutputStream out = new FileOutputStream("file.ttl")) {
+				Rio.write(model, out, RDFFormat.TURTLE);
+			}
+		}
+        return byteArrayOutputStream.toString(StandardCharsets.UTF_8);
+
+    }
+
+    public static List<Object> createInstances(String rdfFile, List<Class> classes) throws RDFParseException,
+			UnsupportedRDFormatException, FileNotFoundException, IOException, IllegalAccessException,
+			IllegalArgumentException, InvocationTargetException, InstantiationException {
+        Model model = Rio.parse(new FileReader(rdfFile), "", RDFFormat.TURTLE);
+
+        List<Object> obj = new ArrayList();
+
+        String id = "";
+        int Count = -1;
+        Boolean a = true;
+
+        for (Statement st : model) {
+            String currentSub = st.getSubject().toString();
+            String currentPred = st.getPredicate().stringValue();
+            String currentObj = st.getObject().stringValue();
+            Class currentClass = null;
+            String pred = "";
+
+            for (Class c : classes) {
+                RDFObject anno = (RDFObject) c.getAnnotation(RDFObject.class);
+                if (currentSub.contains(anno.value())) {
+                    currentClass = c;
+                    break;
+                }
+            }
+
+            if (currentClass != null) {
+                Method[] methods = currentClass.getDeclaredMethods();
+                if (currentSub == id) {
+                    // Loop through the methods and check if it fits the currentObject
+                    for (Method m : methods) {
+                        Class<?>[] types = m.getParameterTypes();
+                        if (types.length < 1) {
+                            continue;
+                        }
+
+                        RDF l = m.getAnnotation(RDF.class);
+
+                        if (l != null) {
+                            pred = l.value();
+                        } else {
+                            continue;
+                        }
+                        if (pred.equals(currentPred)) {
+                            m.invoke(obj.get(Count), PojoRdfHelper.getDatatype(types[0], currentObj));
+                        }
+                    }
+
+                } else {
+                    // Handle a new Object and store it in an array
+                    id = currentSub;
+                    obj.add(currentClass.newInstance());
+                    Count++;
+
+                    for (Method m : methods) {
+                        Class<?>[] types = m.getParameterTypes();
+                        if (types.length < 1) {
+                            continue;
+                        }
+
+                        if (m.getAnnotation(RDFSubject.class) != null) {
+                            m.invoke(obj.get(Count), PojoRdfHelper.getDatatype(types[0], currentSub));
+                            continue;
+                        }
+
+                        RDF l = m.getAnnotation(RDF.class);
+
+                        if (l != null) {
+                            pred = l.value();
+                        }
+
+                        if (pred.equals(currentPred)) {
+                            m.invoke(obj.get(Count), PojoRdfHelper.getDatatype(types[0], currentObj));
+                        }
+
+                    }
+                }
+            }
+        }
+        return obj;
+    }
+
+}
