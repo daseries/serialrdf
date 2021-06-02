79fb35e5f435bc358d7a55e61e3cb44fad2dfc7d
daseries <sebastian.ries@st.ovgu.de>
2021-06-02 14:17:46 +0200

    02.06.

--- src/main/java/anno/Helper.java
+++ src/main/java/anno/Helper.java
@@ -1,31 +1,143 @@
 package anno;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.util.ModelBuilder;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
 
 public class Helper {
-
-	public static Method[] getMethods(String currentSub) {
-		Class BookClass = Book.class;
-		Method[] methodsB = BookClass.getDeclaredMethods();
-		RDFObject BookAnno = (RDFObject) BookClass.getAnnotation(RDFObject.class);
-		
-		if(currentSub.contains(BookAnno.value())) {
-			return methodsB;
-		} 
-		
-		Class OfferClass = Offer.class;
-		Method[] methodsO = OfferClass.getDeclaredMethods();
-		return methodsO;
-		
-	}
 	
-	public static Object getData(Type type, String cur) {
+	public static Object getDatatype(Type type, String cur) {
 		if(type.getTypeName() == "java.lang.Double") {
 			return Double.parseDouble(cur);
 		}
 		return cur;		
 	}
 	
+	
+	public static void convertToRDF(List<Object> objects) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IOException, NoSuchMethodException, SecurityException {
+		ModelBuilder mb = new ModelBuilder();
+		for(Object o: objects) {
+			Class c = o.getClass();
+			RDFObject objectAnnotation = (RDFObject) c.getAnnotation(RDFObject.class);
+			RDFSubject subjectAnnotation = (RDFSubject) c.getAnnotation(RDFSubject.class);
+			RDFNamespaces namespaces = (RDFNamespaces) c.getAnnotation(RDFNamespaces.class);
+			mb.setNamespace(namespaces.value()[0], namespaces.value()[1]);
+			
+			Method methods[] = c.getDeclaredMethods();
+			Method getID = c.getDeclaredMethod("getId");
+			String subject = (String) getID.invoke(o);
+			for(Method m: methods) {
+				RDF methodAnnotation= m.getAnnotation(RDF.class);
+				if(methodAnnotation != null && m.getParameterCount() < 1) {
+					mb.add(subject, methodAnnotation.value().toString(), m.invoke(o));
+				} else {
+					continue;
+				}
+			}
+		}
+		Model model = mb.build();
+		FileOutputStream out = new FileOutputStream("file.ttl");
+				try {
+				  Rio.write(model, out, RDFFormat.TURTLE);
+				}
+				finally {
+				  out.close();
+				}
+		
+	}
+	
+	public static List<Object> createInstances(String rdfFile, List<Class> classes) throws RDFParseException, UnsupportedRDFormatException, FileNotFoundException, IOException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
+		Model model = Rio.parse(new FileReader(rdfFile),"",RDFFormat.TURTLE);
+		
+		List<Object> obj = new ArrayList();
+		
+		String id = "";
+		int Count = -1;
+		Boolean a = true;
+		
+		for(Statement st: model) {
+			String currentSub = st.getSubject().toString();
+			String currentPred = st.getPredicate().stringValue();
+			String currentObj = st.getObject().stringValue();
+			Class currentClass = null;
+			String pred = "";
+			
+			for(Class c: classes) {
+				RDFObject anno = (RDFObject) c.getAnnotation(RDFObject.class);
+				if(currentSub.contains(anno.value())) {
+					currentClass = c;
+					break;
+				}
+			}
+			
+			if(currentClass != null) {
+			Method[] methods = currentClass.getDeclaredMethods();
+				if(currentSub == id) {
+					// Loop through the methods and check if it fits the currentObject
+					for(Method m: methods) {
+						Class<?>[] types = m.getParameterTypes();
+						if(types.length < 1) {
+							continue;
+						}
+						
+						RDF l = m.getAnnotation(RDF.class);
+						
+						if(l != null) {
+							pred = l.value();
+						} else {
+							continue;
+						}
+						if(pred.equals(currentPred)) {
+							m.invoke(obj.get(Count), Helper.getDatatype(types[0], currentObj));
+						}
+					}
+					
+				} else {
+					// Handle a new Object and store it in an array
+					id = currentSub;
+					obj.add(currentClass.newInstance());
+					Count++;
+					
+					for(Method m: methods) {	
+						Class<?>[] types = m.getParameterTypes();
+						if(types.length < 1) {
+							continue;
+						}
+								
+						if(m.getAnnotation(RDFSubject.class) != null) {
+							m.invoke(obj.get(Count), Helper.getDatatype(types[0], currentSub));
+							continue;
+						}
+
+						RDF l = m.getAnnotation(RDF.class);
+
+						if(l != null) {
+							pred = l.value();
+						}
+
+						if(pred.equals(currentPred)) {
+							m.invoke(obj.get(Count), Helper.getDatatype(types[0], currentObj));
+						}
+						
+					}
+				}
+			}
+		}
+		return obj;
+	}
 	
 }
--- src/main/java/anno/Main.java
+++ src/main/java/anno/Main.java
@@ -26,119 +26,21 @@ import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
 public class Main {
 
 	
-	public static void main(String[] args) throws RDFParseException, UnsupportedRDFormatException, FileNotFoundException, IOException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public static void main(String[] args) throws RDFParseException, UnsupportedRDFormatException, FileNotFoundException, IOException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException, NoSuchMethodException, SecurityException {
 		// TODO Auto-generated method stub
 		Model model = Rio.parse(new FileReader("book.ttl"),"",RDFFormat.TURTLE);
 
-		IRI name = SimpleValueFactory.getInstance().createIRI("http://schema.org/name");
-		IRI b = SimpleValueFactory.getInstance().createIRI("http://example.org/book/");
-		Model books = model.filter(b, null, null );
-
-		List<Book> Books = new ArrayList();
-		List<Offer> Offers = new ArrayList();
+		List<Class> classes = new ArrayList();
 		
-		Class BookClass = Book.class;
-		RDFObject BookAnno = (RDFObject) BookClass.getAnnotation(RDFObject.class);
+		classes.add(Book.class);
+		classes.add(Offer.class);
 		
-		//System.out.println(BookAnno);
-		//System.out.println(methods[0].getAnnotation(RDF.class));
+		List<Object> objects = Helper.createInstances("book.ttl", classes);
 		
-		String id = "";
-		int bCount = -1;
-		int oCount = -1;
-		Boolean a = true;
-		
-		
-		for(Statement st: model) {
-			String currentSub = st.getSubject().toString();
-			String currentPred = st.getPredicate().stringValue();
-			String currentObj = st.getObject().stringValue();
-			Method[] methods = Helper.getMethods(currentSub);
-			String pred = "";
-			
-				
-				//System.out.println(currentSub);
-				if(currentSub == id) {
-					// Loop through the methods and check if it fits the currentObject
-					for(Method m: methods) {
-						Class<?>[] types = m.getParameterTypes();
-						if(types.length < 1) {
-							continue;
-						}
-						
-						//System.out.println(st.getSubject().toString());
-						RDF l = m.getAnnotation(RDF.class);
-						
-						if(l != null) {
-							pred = l.value();
-							//System.out.println(currentPred + " " + pred);
-						}
-						if(pred.equals(currentPred)) {
-							//System.out.println(pred + " " + currentPred);
-							//Invoke the correct method
-							if(currentSub.contains(BookAnno.value())) {
-									m.invoke(Books.get(bCount), Helper.getData(types[0], currentObj));
-								
-							} else {
-									m.invoke(Offers.get(oCount), Helper.getData(types[0], currentObj));
-
-							}
-							
-						}
-
-						//System.out.println(m.getName());
-						
-						//m.invoke(Books.get(0), "");
-					}
-					
-				} else {
-					// Handle a new Object and store it in an array
-					id = currentSub;
-					if(currentSub.contains(BookAnno.value())) {
-					Books.add(new Book());
-					Books.get(bCount+1).setId(currentSub);
-					bCount++;
-					} else {
-					Offers.add(new Offer());
-					Offers.get(oCount+1).setId(currentSub);
-					oCount++;
-					}
-					for(Method m: methods) {
-						
-						Class<?>[] types = m.getParameterTypes();
-						if(types.length < 1) {
-							continue;
-						}
-						RDF l = m.getAnnotation(RDF.class);
-
-						if(l != null) {
-							pred = l.value();
-						}
-
-						if(pred.equals(currentPred)) {
-							if(currentSub.contains(BookAnno.value())) {
-								m.invoke(Books.get(bCount), Helper.getData(types[0], currentObj));
-								pred = "";
-							} else {
-								
-								m.invoke(Offers.get(oCount), Helper.getData(types[0], currentObj));
-								pred = "";
-							}
-						}
-					}
-					
-				}
-			
-			
-			//System.out.println(st.getSubject() + " " + st.getPredicate().toString() + " " + st.getObject().toString() );
+		for(Object o: objects) {
+			System.out.println(o.toString());
 		}
 		
-		for(Book bo: Books) {
-			System.out.println(bo.toString());
-		}
-		for(Offer oo: Offers) {
-			System.out.println(oo.toString());
-		}
+		Helper.convertToRDF(objects);
 	}
-
 }
